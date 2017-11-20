53947934fc741f05546de3d98da5972f3a857085
ModestMoose <ModestMoose@users.noreply.github.com>
2017-11-20 15:14:30 -0500

    Integer version

---
+++ Source.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+
+	//variable declarations
+	int numint;
+	float numfloat;
+	FILE *inFile;
+
+	//open the file to read from
+	inFile = fopen("number.txt", "r");
+
+	//exit the profram if opening file is unsuccessful
+	if (inFile == NULL)
+	{
+		printf("Error: Can't open file\n");
+		printf("Make sure there's a file named number.txt in the same folder as .c file\n");
+		exit(101);
+	}
+
+	//
+	//get one number from file and save it as an integer
+	fscanf(inFile, "%d", &numint);
+
+	//print integer to STDOUT
+	printf("%d\n", numint);
+
+	/*
+	//get one number from file and save it as a float
+	fscanf(inFile, "%f", &numfloat);
+
+	//print float to STDOut
+	printf("%f\n", numfloat);
+	*/
+
+	//close the file
+	fclose (inFile);
+	
+	//exit program
+	return 0;
+}
